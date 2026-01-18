1dbf6925742898f84d68f051e87ba9f331ab41ff
Lê Nguyễn An Ninh <leninh2004@gmail.com>
2026-01-18 19:57:15 +0700

    Restructure: move pages to lib/src and add src folders

---
+++ lib/main_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/search_page.dart';
+import 'pages/profile_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [HomePage(), SearchPage(), ProfilePage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
